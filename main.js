dd8fe480d80d691cb774a8c666af669fa9cd799a
krishnaloqbox <142591199+krishnaloqbox@users.noreply.github.com>
2023-08-29 16:48:45 +0530

    Update main.js to domain

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const setPartnerCookie=(hostDomain, APIURL)=>{
     };
 
     function setCookie(cookieName, cookieValue) {
-        Cookies.set(cookieName, cookieValue, {expires: 30, path: '/', origin: `${hostDomain}`})
+        Cookies.set(cookieName, cookieValue, {expires: 30, path: '/', domain: `${hostDomain}`})
     }
 
     const urlSearchParams = new URLSearchParams(window.location.search);
